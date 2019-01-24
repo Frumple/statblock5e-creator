70e22208b7c1c948961d70d7bb96c230d691fc6d
Frumple <frumple1@gmail.com>
2019-01-23 16:31:22 -0800

    Standardize button styling. Add button that toggles the visibility of empty attributes in the advanced stats container.

--- src/js/containers/advanced-stats.js
+++ src/js/containers/advanced-stats.js
@@ -11,24 +11,47 @@ export default class AdvancedStats extends CustomAutonomousElement {
     this.skillsSection = document.querySelector('skills-section');
     this.challengeSection = document.querySelector('challenge-section');
 
+    this.toggleEmptyAttributesContainer = this.shadowRoot.getElementById('toggle-empty-attributes-container');
+    this.toggleEmptyAttributesButton = this.shadowRoot.getElementById('toggle-empty-attributes-button');
+
+    this.addEventListener('mouseenter', () => {
+      this.toggleEmptyAttributesContainer.classList.remove('toggle-empty-attributes-container_hidden');
+    });
+
+    this.addEventListener('mouseleave', () => {
+      this.toggleEmptyAttributesContainer.classList.add('toggle-empty-attributes-container_hidden');
+    });
+
+    this.toggleEmptyAttributesButton.addEventListener('click', () => {
+      this.toggleEmptyAttributes();
+    });
+  }
+
+  toggleEmptyAttributes() {
+    let button = this.toggleEmptyAttributesButton;
+
     let hiddenSectionsWhenEmpty = [
       this.savingThrowsSection,
       this.skillsSection
     ];
 
-    this.addEventListener('mouseenter', () => {
-      hiddenSectionsWhenEmpty.forEach( (section) => {
-        section.showElements.section.classList.remove('section_empty-hidden');
-      });
-    });
-
-    this.addEventListener('mouseleave', () => {
+    if('emptyAttributesVisible' in button.dataset) {
       hiddenSectionsWhenEmpty.forEach( (section) => {
         let showSection = section.showElements.section;
         if (showSection.classList.contains('section_empty')) {
           showSection.classList.add('section_empty-hidden');
         }
       });
-    });
+
+      button.textContent = 'Show Empty Attributes';
+      delete button.dataset.emptyAttributesVisible;
+    } else {
+      hiddenSectionsWhenEmpty.forEach( (section) => {
+        section.showElements.section.classList.remove('section_empty-hidden');
+      });
+
+      button.textContent = 'Hide Empty Attributes';
+      button.dataset.emptyAttributesVisible = '';
+    }
   }
 }
