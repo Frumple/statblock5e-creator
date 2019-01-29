a48151d600157d82c55ad2406daf7e3c3de34e7b
Frumple <frumple1@gmail.com>
2019-01-29 14:04:08 -0800

    Extract attribute list base section from languages section.

---
+++ src/js/sections/attribute-list-section.js
@@ -0,0 +1,104 @@
+import * as sectionModule from '/src/js/base/section.js';
+
+export default class AttributeListSection extends sectionModule.Section {
+  static get templatePaths() {
+    return super.templatePaths.set(
+      'attribute-list-section',
+      'src/html/sections/attribute-list-section.html');
+  }
+
+  constructor(templatePaths, headerText) {
+    super(templatePaths,
+          AttributeListShowElements,
+          AttributeListEditElements);
+
+    this.showElements.header.textContent = headerText;
+    this.editElements.label.textContent = `${headerText}:`;
+
+    this.editElements.input.addEventListener('keydown', (keyEvent) => {
+      if (keyEvent.key === 'Enter') {
+        keyEvent.preventDefault();
+
+        this.addItemFromInput();
+      }
+    });
+
+    this.editElements.addButton.addEventListener('click', () => {
+      this.addItemFromInput();
+    });
+
+    this.addEventListener('attributeListItemRemoved', (event) => {
+      let itemText = event.detail.itemText;
+      this.editElements.datalist.setOptionEnabled(itemText, true);
+    });
+  }
+
+  addItemFromInput() {
+    let text = this.editElements.input.value.trim();
+    this.editElements.input.value = text;
+
+    this.errorMessages.clear();
+    if (text === '') {
+      this.errorMessages.add(this.editElements.input, 'Cannot add a blank item.');
+    } else if(this.editElements.list.contains(text)) {
+      this.errorMessages.add(this.editElements.input, 'Cannot add a duplicate item.');
+    }
+    if (this.errorMessages.any()) {
+      return;
+    }
+
+    this.addItem(text);
+
+    this.editElements.input.value = '';
+    this.editElements.input.select();
+  }
+
+  addItem(text) {
+    this.editElements.list.addItem(text);
+    this.editElements.datalist.setOptionEnabled(text, false);
+  }
+
+  get initialSelectedEditElement() {
+    return this.editElements.input;
+  }
+
+  checkForErrors() {
+
+  }
+
+  update() {
+    this.editElements.input.value = '';
+
+    let text = '';
+    let itemTextList = this.editElements.list.itemTextList;
+    for (const itemText of itemTextList) {
+      if (text === '') {
+        text += itemText;
+      } else {
+        text += `, ${itemText}`;
+      }
+    }
+
+    this.showElements.text.textContent = text;
+  }
+}
+
+class AttributeListShowElements extends sectionModule.ShowElements {
+  constructor(shadowRoot) {
+    super(shadowRoot);
+    this.header = shadowRoot.getElementById('attribute-list-header');
+    this.text = shadowRoot.getElementById('attribute-list-text');
+  }
+}
+
+class AttributeListEditElements extends sectionModule.EditElements {
+  constructor(shadowRoot) {
+    super(shadowRoot);
+
+    this.label = shadowRoot.getElementById('attribute-list-label');
+    this.input = shadowRoot.getElementById('attribute-list-input');
+    this.addButton = shadowRoot.getElementById('attribute-list-add-button');
+    this.list = shadowRoot.getElementById('attribute-list');
+    this.datalist = shadowRoot.getElementById('attribute-list-datalist');
+  }
+}
--- src/js/sections/languages-section.js
+++ src/js/sections/languages-section.js
@@ -1,6 +1,6 @@
-import * as sectionModule from '/src/js/base/section.js';
+import AttributeListSection from '/src/js/sections/attribute-list-section.js';
 
-export default class LanguagesSection extends sectionModule.Section {
+export default class LanguagesSection extends AttributeListSection {
   static get elementName() { return 'languages-section'; }
   static get templatePaths() {
     return super.templatePaths.set(
@@ -10,107 +10,31 @@ export default class LanguagesSection extends sectionModule.Section {
 
   constructor() {
     super(LanguagesSection.templatePaths,
-          LanguagesShowElements,
-          LanguagesEditElements);
+          'Languages');
 
     this.listInitialized = false;
-
-    this.editElements.input.addEventListener('keydown', (keyEvent) => {
-      if (keyEvent.key === 'Enter') {
-        keyEvent.preventDefault();
-
-        this.addItemFromInput();
-      }
-    });
-
-    this.editElements.addButton.addEventListener('click', () => {
-      this.addItemFromInput();
-    });
-
-    this.addEventListener('attributeListItemRemoved', (event) => {
-      let itemText = event.detail.itemText;
-      this.editElements.datalist.setOptionEnabled(itemText, true);
-    });
   }
 
   connectedCallback() {
     if (this.isConnected) {
       if (! this.listInitialized) {
+
         this.addItem('Common');
+        this.update();
         this.listInitialized = true;
       }
     }
   }
 
-  addItemFromInput() {
-    let text = this.editElements.input.value.trim();
-    this.editElements.input.value = text;
-
-    this.errorMessages.clear();
-    if (text === '') {
-      this.errorMessages.add(this.editElements.input, 'Cannot add a blank item.');
-    } else if(this.editElements.list.contains(text)) {
-      this.errorMessages.add(this.editElements.input, 'Cannot add a duplicate item.');
-    }
-    if (this.errorMessages.any()) {
-      return;
-    }
-
-    this.addItem(text);
-
-    this.editElements.input.value = '';
-    this.editElements.input.select();
-  }
-
-  addItem(text) {
-    this.editElements.list.addItem(text);
-    this.editElements.datalist.setOptionEnabled(text, false);
-  }
-
-  get initialSelectedEditElement() {
-    return this.editElements.input;
-  }
-
-  checkForErrors() {
-
-  }
-
   update() {
-    this.editElements.input.value = '';
+    super.update();
 
-    let text = '';
-    let itemTextList = this.editElements.list.itemTextList;
-    for (const itemText of itemTextList) {
-      if (text === '') {
-        text += itemText;
-      } else {
-        text += `, ${itemText}`;
-      }
-    }
+    let text = this.showElements.text.textContent;
     if (text === '') {
       // This is an EM dash (U+2014).
       // This appears significantly wider than a normal dash.
       text = '—';
     }
-
     this.showElements.text.textContent = text;
-  }
-}
-
-class LanguagesShowElements extends sectionModule.ShowElements {
-  constructor(shadowRoot) {
-    super(shadowRoot);
-    this.text = shadowRoot.getElementById('languages-text');
-  }
-}
-
-class LanguagesEditElements extends sectionModule.EditElements {
-  constructor(shadowRoot) {
-    super(shadowRoot);
-
-    this.input = shadowRoot.getElementById('languages-input');
-    this.addButton = shadowRoot.getElementById('languages-add-button');
-    this.list = shadowRoot.getElementById('languages-list');
-    this.datalist = shadowRoot.getElementById('languages-datalist');
   }
 }
