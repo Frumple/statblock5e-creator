641c78f246cf12cc99513f1d766f313a044b0d3e
Frumple <frumple1@gmail.com>
2019-02-07 12:57:36 -0800

    Fix nesting of describe clauses in tests.

--- src/js/sections/armor-class-section.test.js
+++ src/js/sections/armor-class-section.test.js
@@ -17,143 +17,143 @@ beforeEach(() => {
 });
 
 describe('when the show section is clicked', () => {
-  it('should switch to edit mode and focus on the armor class field', () => {
+  beforeEach(() => {
     armorClassSection.showElements.section.click(); 
-    
+  });
+
+  it('should switch to edit mode and focus on the armor class field', () => {    
     expect(armorClassSection).toBeInMode('edit');
     expect(armorClassSection.editElements.armorClass).toHaveFocus();
   });
-});
 
-describe('when the custom text checkbox is checked', () => {
-  beforeEach(() => {
-    armorClassSection.showElements.section.click(); 
-    armorClassSection.editElements.useCustom.click();
-  });
-
-  it('should enable the custom text field, disable all other fields, and focus on the custom text field', () => {
-    expect(armorClassSection.editElements.armorClass).toBeDisabled();
-    expect(armorClassSection.editElements.armorType).toBeDisabled();
-    expect(armorClassSection.editElements.shield).toBeDisabled();
-    expect(armorClassSection.editElements.customText).not.toBeDisabled();
-
-    expect(armorClassSection.editElements.customText).toHaveFocus();
-  });
-
-  describe('and the save button is clicked', () => {
-    it('should switch to show mode and save the custom text', () => {
-      let customText = '14 (natural armor), 11 while prone';
-      inputValue(armorClassSection.editElements.customText, customText);
-
-      armorClassSection.editElements.saveAction.click();
-
-      expect(armorClassSection).toBeInMode('show');
-      expect(armorClassSection.showElements.text).toHaveTextContent(customText);
+  describe('and the custom text checkbox is checked', () => {
+    beforeEach(() => {
+      armorClassSection.editElements.useCustom.click();
     });
 
-    it('should display an error if the custom text field is blank', () => {
-      inputValue(armorClassSection.editElements.customText, '');
+    it('should enable the custom text field, disable all other fields, and focus on the custom text field', () => {
+      expect(armorClassSection.editElements.armorClass).toBeDisabled();
+      expect(armorClassSection.editElements.armorType).toBeDisabled();
+      expect(armorClassSection.editElements.shield).toBeDisabled();
+      expect(armorClassSection.editElements.customText).not.toBeDisabled();
 
-      armorClassSection.editElements.saveAction.click();
-
-      expect(armorClassSection).toBeInMode('edit');
-      expect(armorClassSection).toHaveSingleError(
-        armorClassSection.editElements.customText,
-        'Armor Class Custom Text cannot be blank.');
+      expect(armorClassSection.editElements.customText).toHaveFocus();
     });
 
-    it('should display only one error if the armor class and custom text fields are both blank', () => {
-      inputValue(armorClassSection.editElements.armorClass, '');
-      inputValue(armorClassSection.editElements.customText, '');
+    describe('and the save button is clicked', () => {
+      it('should switch to show mode and save the custom text', () => {
+        let customText = '14 (natural armor), 11 while prone';
+        inputValue(armorClassSection.editElements.customText, customText);
 
-      armorClassSection.editElements.saveAction.click();
+        armorClassSection.editElements.saveAction.click();
 
-      expect(armorClassSection).toBeInMode('edit');
-      expect(armorClassSection).toHaveSingleError(
-        armorClassSection.editElements.customText,
-        'Armor Class Custom Text cannot be blank.');
-    });
-  });
-});
+        expect(armorClassSection).toBeInMode('show');
+        expect(armorClassSection.showElements.text).toHaveTextContent(customText);
+      });
 
-describe('when the custom text checkbox is unchecked', () => {
-  beforeEach(() => {
-    armorClassSection.showElements.section.click(); 
-    armorClassSection.editElements.useCustom.click();
-    armorClassSection.editElements.useCustom.click();
-  });
+      it('should display an error if the custom text field is blank', () => {
+        inputValue(armorClassSection.editElements.customText, '');
 
-  it('should disable the custom text field, enable all other fields, and focus on the armor class field', () => {
-    expect(armorClassSection.editElements.armorClass).not.toBeDisabled();
-    expect(armorClassSection.editElements.armorType).not.toBeDisabled();
-    expect(armorClassSection.editElements.shield).not.toBeDisabled();
-    expect(armorClassSection.editElements.customText).toBeDisabled();
+        armorClassSection.editElements.saveAction.click();
 
-    expect(armorClassSection.editElements.armorClass).toHaveFocus();
-  });
+        expect(armorClassSection).toBeInMode('edit');
+        expect(armorClassSection).toHaveSingleError(
+          armorClassSection.editElements.customText,
+          'Armor Class Custom Text cannot be blank.');
+      });
 
-  describe('and the save button is clicked', () => {
-    it('should switch to show mode and save the armor class only', () => {
-      inputValue(armorClassSection.editElements.armorClass, 7);
+      it('should display only one error if the armor class and custom text fields are both blank', () => {
+        inputValue(armorClassSection.editElements.armorClass, '');
+        inputValue(armorClassSection.editElements.customText, '');
 
-      armorClassSection.editElements.saveAction.click();
+        armorClassSection.editElements.saveAction.click();
 
-      expect(armorClassSection).toBeInMode('show');
-      expect(armorClassSection.showElements.text).toHaveTextContent('7');
-    });
-
-    it('should switch to show mode and save the armor class and armor type', () => {
-      inputValue(armorClassSection.editElements.armorClass, 21);
-      inputValue(armorClassSection.editElements.armorType, 'natural armor');
-
-      armorClassSection.editElements.saveAction.click();
-
-      expect(armorClassSection).toBeInMode('show');
-      expect(armorClassSection.showElements.text).toHaveTextContent('21 (natural armor)');
-    });
-
-    it('should switch to show mode and save the armor class and shield', () => {
-      inputValue(armorClassSection.editElements.armorClass, 12);      
-      armorClassSection.editElements.shield.click();
-
-      armorClassSection.editElements.saveAction.click();
-
-      expect(armorClassSection).toBeInMode('show');
-      expect(armorClassSection.showElements.text).toHaveTextContent('12 (shield)');
-    });
-
-    it('should switch to show mode and save the armor class, armor type, and shield', () => {
-      inputValue(armorClassSection.editElements.armorClass, 16);
-      inputValue(armorClassSection.editElements.armorType, 'chain shirt');
-      armorClassSection.editElements.shield.click();
-
-      armorClassSection.editElements.saveAction.click();
-
-      expect(armorClassSection).toBeInMode('show');
-      expect(armorClassSection.showElements.text).toHaveTextContent('16 (chain shirt, shield)');
-    });
-
-    it('should display an error if the armor class field is blank', () => {
-      inputValue(armorClassSection.editElements.armorClass, '');
-
-      armorClassSection.editElements.saveAction.click();
-
-      expect(armorClassSection).toBeInMode('edit');
-      expect(armorClassSection).toHaveSingleError(
-        armorClassSection.editElements.armorClass,
-        'Armor Class must be a valid number.');
-    });
-
-    it('should display only one error if the armor class and custom text fields are both blank', () => {
-      inputValue(armorClassSection.editElements.armorClass, '');
-      inputValue(armorClassSection.editElements.customText, '');
-
-      armorClassSection.editElements.saveAction.click();
-
-      expect(armorClassSection).toBeInMode('edit');
-      expect(armorClassSection).toHaveSingleError(
-        armorClassSection.editElements.armorClass,
-        'Armor Class must be a valid number.');
+        expect(armorClassSection).toBeInMode('edit');
+        expect(armorClassSection).toHaveSingleError(
+          armorClassSection.editElements.customText,
+          'Armor Class Custom Text cannot be blank.');
+      });
     });
   });
+
+  describe('and the custom text checkbox is unchecked', () => {
+    beforeEach(() => {
+      armorClassSection.editElements.useCustom.click();
+      armorClassSection.editElements.useCustom.click();
+    });
+
+    it('should disable the custom text field, enable all other fields, and focus on the armor class field', () => {
+      expect(armorClassSection.editElements.armorClass).not.toBeDisabled();
+      expect(armorClassSection.editElements.armorType).not.toBeDisabled();
+      expect(armorClassSection.editElements.shield).not.toBeDisabled();
+      expect(armorClassSection.editElements.customText).toBeDisabled();
+
+      expect(armorClassSection.editElements.armorClass).toHaveFocus();
+    });
+
+    describe('and the save button is clicked', () => {
+      it('should switch to show mode and save the armor class only', () => {
+        inputValue(armorClassSection.editElements.armorClass, 7);
+
+        armorClassSection.editElements.saveAction.click();
+
+        expect(armorClassSection).toBeInMode('show');
+        expect(armorClassSection.showElements.text).toHaveTextContent('7');
+      });
+
+      it('should switch to show mode and save the armor class and armor type', () => {
+        inputValue(armorClassSection.editElements.armorClass, 21);
+        inputValue(armorClassSection.editElements.armorType, 'natural armor');
+
+        armorClassSection.editElements.saveAction.click();
+
+        expect(armorClassSection).toBeInMode('show');
+        expect(armorClassSection.showElements.text).toHaveTextContent('21 (natural armor)');
+      });
+
+      it('should switch to show mode and save the armor class and shield', () => {
+        inputValue(armorClassSection.editElements.armorClass, 12);      
+        armorClassSection.editElements.shield.click();
+
+        armorClassSection.editElements.saveAction.click();
+
+        expect(armorClassSection).toBeInMode('show');
+        expect(armorClassSection.showElements.text).toHaveTextContent('12 (shield)');
+      });
+
+      it('should switch to show mode and save the armor class, armor type, and shield', () => {
+        inputValue(armorClassSection.editElements.armorClass, 16);
+        inputValue(armorClassSection.editElements.armorType, 'chain shirt');
+        armorClassSection.editElements.shield.click();
+
+        armorClassSection.editElements.saveAction.click();
+
+        expect(armorClassSection).toBeInMode('show');
+        expect(armorClassSection.showElements.text).toHaveTextContent('16 (chain shirt, shield)');
+      });
+
+      it('should display an error if the armor class field is blank', () => {
+        inputValue(armorClassSection.editElements.armorClass, '');
+
+        armorClassSection.editElements.saveAction.click();
+
+        expect(armorClassSection).toBeInMode('edit');
+        expect(armorClassSection).toHaveSingleError(
+          armorClassSection.editElements.armorClass,
+          'Armor Class must be a valid number.');
+      });
+
+      it('should display only one error if the armor class and custom text fields are both blank', () => {
+        inputValue(armorClassSection.editElements.armorClass, '');
+        inputValue(armorClassSection.editElements.customText, '');
+
+        armorClassSection.editElements.saveAction.click();
+
+        expect(armorClassSection).toBeInMode('edit');
+        expect(armorClassSection).toHaveSingleError(
+          armorClassSection.editElements.armorClass,
+          'Armor Class must be a valid number.');
+      });
+    });
+  });  
 });
--- src/js/sections/heading-section.test.js
+++ src/js/sections/heading-section.test.js
@@ -14,89 +14,87 @@ beforeEach(() => {
 });
 
 describe('when the show section is clicked', () => {
-  it('should switch to edit mode and focus on the title field', () => {
-    headingSection.showElements.section.click(); 
-    
-    expect(headingSection).toBeInMode('edit');
-    expect(headingSection.editElements.title).toHaveFocus();
-  });
-});
-
-describe('when the save button is clicked', () => {
   beforeEach(() => {
     headingSection.showElements.section.click(); 
   });
 
-  it('should switch to show mode and save the creature name, size, type, and alignment', () => {
-    inputValue(headingSection.editElements.title, 'Beholder');
-    inputValue(headingSection.editElements.size, 'Large');
-    inputValue(headingSection.editElements.type, 'aberration');
-    inputValue(headingSection.editElements.alignment, 'lawful evil');
-  
-    headingSection.editElements.saveAction.click();
-
-    expect(headingSection).toBeInMode('show');
-    expect(headingSection.showElements.title).toHaveTextContent('Beholder');
-    expect(headingSection.showElements.subtitle).toHaveTextContent('Large aberration, lawful evil');
-  });
-
-  it('should capitalize the first letter in the creature name', () => {
-    inputValue(headingSection.editElements.title, 'young red dragon');
-
-    headingSection.editElements.saveAction.click();
-
-    expect(headingSection).toBeInMode('show');
-    expect(headingSection.showElements.title).toHaveTextContent('Young red dragon');
-  });
-
-  it('should trim whitespace from the creature name and type', () => {
-    inputValue(headingSection.editElements.title, '  Purple Worm ');
-    inputValue(headingSection.editElements.size, 'Gargantuan');
-    inputValue(headingSection.editElements.type, '    monstrosity        ');
-    inputValue(headingSection.editElements.alignment, 'unaligned');
-
-    headingSection.editElements.saveAction.click();
-
-    expect(headingSection).toBeInMode('show');
-    expect(headingSection.showElements.title).toHaveTextContent('Purple Worm');
-    expect(headingSection.showElements.subtitle).toHaveTextContent('Gargantuan monstrosity, unaligned');
-  });
-
-  it('should display an error if the creature name field is blank', () => {
-    inputValue(headingSection.editElements.title, '');
-
-    headingSection.editElements.saveAction.click();
-
+  it('should switch to edit mode and focus on the title field', () => {
     expect(headingSection).toBeInMode('edit');
-    expect(headingSection).toHaveSingleError(
-      headingSection.editElements.title,
-      'Creature Name cannot be blank.');
+    expect(headingSection.editElements.title).toHaveFocus();
   });
 
-  it('should display an error if the creature type field is blank', () => {
-    inputValue(headingSection.editElements.type, '');
+  describe('and fields are populated and the save button is clicked', () => {
+    it('should switch to show mode and save the creature name, size, type, and alignment', () => {
+      inputValue(headingSection.editElements.title, 'Beholder');
+      inputValue(headingSection.editElements.size, 'Large');
+      inputValue(headingSection.editElements.type, 'aberration');
+      inputValue(headingSection.editElements.alignment, 'lawful evil');
+    
+      headingSection.editElements.saveAction.click();
 
-    headingSection.editElements.saveAction.click();
+      expect(headingSection).toBeInMode('show');
+      expect(headingSection.showElements.title).toHaveTextContent('Beholder');
+      expect(headingSection.showElements.subtitle).toHaveTextContent('Large aberration, lawful evil');
+    });
 
-    expect(headingSection).toBeInMode('edit');
-    expect(headingSection).toHaveSingleError(
-      headingSection.editElements.type,
-      'Creature Type cannot be blank.');
-  });
+    it('should capitalize the first letter in the creature name', () => {
+      inputValue(headingSection.editElements.title, 'young red dragon');
 
-  it('should display both errors if the creature name and creature type fields are both blank', () => {
-    inputValue(headingSection.editElements.title, '');
-    inputValue(headingSection.editElements.type, '');
+      headingSection.editElements.saveAction.click();
 
-    headingSection.editElements.saveAction.click();
+      expect(headingSection).toBeInMode('show');
+      expect(headingSection.showElements.title).toHaveTextContent('Young red dragon');
+    });
 
-    expect(headingSection).toBeInMode('edit');
-    expect(headingSection.errorMessages.errors.length).toBe(2);
-    expect(headingSection).toHaveError(
-      headingSection.editElements.title,
-      'Creature Name cannot be blank.');
-    expect(headingSection).toHaveError(
-      headingSection.editElements.type,
-      'Creature Type cannot be blank.');
+    it('should trim whitespace from the creature name and type', () => {
+      inputValue(headingSection.editElements.title, '  Purple Worm ');
+      inputValue(headingSection.editElements.size, 'Gargantuan');
+      inputValue(headingSection.editElements.type, '    monstrosity        ');
+      inputValue(headingSection.editElements.alignment, 'unaligned');
+
+      headingSection.editElements.saveAction.click();
+
+      expect(headingSection).toBeInMode('show');
+      expect(headingSection.showElements.title).toHaveTextContent('Purple Worm');
+      expect(headingSection.showElements.subtitle).toHaveTextContent('Gargantuan monstrosity, unaligned');
+    });
+
+    it('should display an error if the creature name field is blank', () => {
+      inputValue(headingSection.editElements.title, '');
+
+      headingSection.editElements.saveAction.click();
+
+      expect(headingSection).toBeInMode('edit');
+      expect(headingSection).toHaveSingleError(
+        headingSection.editElements.title,
+        'Creature Name cannot be blank.');
+    });
+
+    it('should display an error if the creature type field is blank', () => {
+      inputValue(headingSection.editElements.type, '');
+
+      headingSection.editElements.saveAction.click();
+
+      expect(headingSection).toBeInMode('edit');
+      expect(headingSection).toHaveSingleError(
+        headingSection.editElements.type,
+        'Creature Type cannot be blank.');
+    });
+
+    it('should display both errors if the creature name and creature type fields are both blank', () => {
+      inputValue(headingSection.editElements.title, '');
+      inputValue(headingSection.editElements.type, '');
+
+      headingSection.editElements.saveAction.click();
+
+      expect(headingSection).toBeInMode('edit');
+      expect(headingSection.errorMessages.errors.length).toBe(2);
+      expect(headingSection).toHaveError(
+        headingSection.editElements.title,
+        'Creature Name cannot be blank.');
+      expect(headingSection).toHaveError(
+        headingSection.editElements.type,
+        'Creature Type cannot be blank.');
+    });
   });
 });
