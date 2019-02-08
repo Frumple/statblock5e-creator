8fd499a98ad7405c9ebab3b3b13337cc3b42a50a
Frumple <frumple1@gmail.com>
2019-02-08 09:12:42 -0800

    Convert some hit points section tests to it.each table syntax.

--- src/js/sections/hit-points-section.test.js
+++ src/js/sections/hit-points-section.test.js
@@ -50,22 +50,55 @@ describe('when the show section is clicked', () => {
       expect(hitPointsSection.editElements.hitDieQuantity).toHaveFocus();
     });
 
-    describe('and the hit die quantity is changed, and the save button is clicked', () => {
-      it('should update the hit points field with the correctly calculated value, and save all the fields', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 5);
+    describe('and the hit die fields are populated and the save button is clicked', () => {
+      describe('update the hit points field, switch to show mode and save all the fields in the following combinations:', () => {
+        /* eslint-disable indent, no-unexpected-multiline */
+        it.each
+        `
+          description                                 | hitDieQuantity | hitDieSize | constitutionScore | expectedConstitutionHitPoints | expectedHitPoints | expectedText
+          ${'no change'}                              | ${1}           | ${8}       | ${10}             | ${0}                          | ${4}              | ${'4 (1d8)'}
+          ${'quantity changed'}                       | ${5}           | ${8}       | ${10}             | ${0}                          | ${22}             | ${'22 (5d8)'}
+          ${'size changed'}                           | ${1}           | ${6}       | ${10}             | ${0}                          | ${3}              | ${'3 (1d6)'}          
+          ${'con modifier changed'}                   | ${1}           | ${8}       | ${15}             | ${2}                          | ${6}              | ${'6 (1d8 + 2)'}
+          ${'quantity + size changed'}                | ${6}           | ${10}      | ${10}             | ${0}                          | ${33}             | ${'33 (6d10)'}
+          ${'quantity + con modifier changed'}        | ${9}           | ${8}       | ${17}             | ${27}                         | ${67}             | ${'67 (9d8 + 27)'}
+          ${'size + con modifier changed'}            | ${1}           | ${4}       | ${8}              | ${-1}                         | ${1}              | ${'1 (1d4 – 1)'}
+          ${'quantity + size + con modifier changed'} | ${17}          | ${10}      | ${21}             | ${85}                         | ${178}            | ${'178 (17d10 + 85)'}
+          ${'air elemental'}                          | ${12}          | ${10}      | ${14}             | ${24}                         | ${90}             | ${'90 (12d10 + 24)'}
+          ${'ankylosaurus'}                           | ${8}           | ${12}      | ${15}             | ${16}                         | ${68}             | ${'68 (8d12 + 16)'}
+          ${'hill giant'}                             | ${10}          | ${12}      | ${19}             | ${40}                         | ${105}            | ${'105 (10d12 + 40)'}
+          ${'swarm of rats'}                          | ${7}           | ${8}       | ${9}              | ${-7}                         | ${24}             | ${'24 (7d8 – 7)'}
+          ${'tarrasque'}                              | ${33}          | ${20}      | ${30}             | ${330}                        | ${676}            | ${'676 (33d20 + 330)'}
+          ${'winged kobold'}                          | ${3}           | ${6}       | ${9}              | ${-3}                         | ${7}              | ${'7 (3d6 – 3)'}
+          ${'minimum values'}                         | ${1}           | ${4}       | ${1}              | ${-5}                         | ${0}              | ${'0 (1d4 – 5)'}
+          ${'maximum values'}                         | ${999}         | ${20}      | ${999}            | ${493506}                     | ${503995}         | ${'503995 (999d20 + 493506)'}
+        `
+        ('$description: {hitDieQuantity="$hitDieQuantity", hitDieSize="$hitDieSize", constitutionScore="$constitutionScore"} => {expectedConstitutionHitPoints="$expectedConstitutionHitPoints", expectedHitPoints="$expectedHitPoints", expectedText="$expectedText"}',
+        ({description, hitDieQuantity, hitDieSize, constitutionScore, expectedConstitutionHitPoints, expectedHitPoints, expectedText}) => { // eslint-disable-line no-unused-vars
+          inputValue(hitPointsSection.editElements.hitDieQuantity, hitDieQuantity);
+          inputValue(hitPointsSection.editElements.hitDieSize, hitDieSize);
 
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(5);
-        expect(HitPoints.hitDieSize).toBe(8);
-        expect(HitPoints.constitutionHitPoints).toBe(0);
-        expect(HitPoints.hitPoints).toBe(22);
+          Abilities.abilities['constitution'].score = constitutionScore;
+          hitPointsSection.updateHitPoints();
 
-        hitPointsSection.editElements.saveAction.click();
+          expect(HitPoints.useHitDie).toBe(true);
+          expect(HitPoints.hitDieQuantity).toBe(hitDieQuantity);
+          expect(HitPoints.hitDieSize).toBe(hitDieSize);
+          expect(HitPoints.constitutionHitPoints).toBe(expectedConstitutionHitPoints);
+          expect(HitPoints.hitPoints).toBe(expectedHitPoints);
 
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('22 (5d8)');
+          expect(hitPointsSection.editElements.hitPoints.value).toBe(`${expectedHitPoints}`);
+
+          hitPointsSection.editElements.saveAction.click();
+
+          expect(hitPointsSection).toBeInMode('show');
+          expect(hitPointsSection.showElements.text).toHaveTextContent(expectedText);
+        });      
+        /* eslint-enable indent, no-unexpected-multiline */
       });
+    });    
 
+    describe('and the hit die quantity is changed, and the save button is clicked', () => {
       it('should display an error if the hit die quantity field is blank', () => {
         inputValue(hitPointsSection.editElements.hitDieQuantity, NaN);
 
@@ -100,129 +133,7 @@ describe('when the show section is clicked', () => {
           hitPointsSection.editElements.hitDieQuantity,
           'Hit Die Quantity must be a valid number.');
       });
-    });
-
-    describe('and the hit die size is changed, and the save button is clicked', () => {
-      it('should update the hit points field with the correctly calculated value, and save all the fields', () => {
-        inputValue(hitPointsSection.editElements.hitDieSize, 6);
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('3');
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(1);
-        expect(HitPoints.hitDieSize).toBe(6);
-        expect(HitPoints.constitutionHitPoints).toBe(0);
-        expect(HitPoints.hitPoints).toBe(3);
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('3 (1d6)');
-      });
-    });
-
-    describe('and the constitution modifier is changed, and the save button is clicked', () => {
-      it('should update the hit points field with the correctly calculated value, and save all the fields', () => {
-        Abilities.abilities['constitution'].score = 15;
-        hitPointsSection.updateHitPoints();
-        
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(1);
-        expect(HitPoints.hitDieSize).toBe(8);
-        expect(HitPoints.constitutionHitPoints).toBe(2);
-        expect(HitPoints.hitPoints).toBe(6);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('6');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('6 (1d8 + 2)');
-      });
-    });
-
-    describe('and the hit die quantity and size are changed, and the save button is clicked', () => {
-      it('should update the hit points field with the correctly calculated value, and save all the fields', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 6);
-        inputValue(hitPointsSection.editElements.hitDieSize, 10);
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(6);
-        expect(HitPoints.hitDieSize).toBe(10);
-        expect(HitPoints.constitutionHitPoints).toBe(0);
-        expect(HitPoints.hitPoints).toBe(33);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('33');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('33 (6d10)');
-      });
-    });
-
-    describe('and the hit die size and constitution modifier are changed, and the save button is clicked', () => {
-      it('should update the hit points field with the correctly calculated value, and save all the fields', () => {
-        inputValue(hitPointsSection.editElements.hitDieSize, 4);
-        Abilities.abilities['constitution'].score = 8;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(1);
-        expect(HitPoints.hitDieSize).toBe(4);
-        expect(HitPoints.constitutionHitPoints).toBe(-1);
-        expect(HitPoints.hitPoints).toBe(1);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('1');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('1 (1d4 – 1)');
-      });
-    });
-
-    describe('and the hit die quantity and constitution modifier are changed, and the save button is clicked', () => {
-      it('should update the hit points field with the correctly calculated value, and save all the fields', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 9);
-        Abilities.abilities['constitution'].score = 17;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(9);
-        expect(HitPoints.hitDieSize).toBe(8);
-        expect(HitPoints.constitutionHitPoints).toBe(27);
-        expect(HitPoints.hitPoints).toBe(67);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('67');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('67 (9d8 + 27)');
-      });
-    });
-
-    describe('and the hit die quantity, size, and constitution modifier are changed, and the save button is clicked', () => {
-      it('should update the hit points field with the correctly calculated value, and save all the fields', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 17);
-        inputValue(hitPointsSection.editElements.hitDieSize, 10);
-        Abilities.abilities['constitution'].score = 21;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(17);
-        expect(HitPoints.hitDieSize).toBe(10);
-        expect(HitPoints.constitutionHitPoints).toBe(85);
-        expect(HitPoints.hitPoints).toBe(178);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('178');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('178 (17d10 + 85)');
-      });
-    });
+    });  
 
     describe('and the "Calculate using Hit Die" checkbox is unchecked, the hit points field is changed, then the checkbox is checked again', () => {
       it('should reset the hit points field to the value calculated from hit die', () => {
@@ -245,128 +156,6 @@ describe('when the show section is clicked', () => {
 
         expect(hitPointsSection.editElements.hitPoints.value).toBe('105');
       });
-    });
-
-    describe('additional examples', () => {
-      it('air elemental', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 12);
-        inputValue(hitPointsSection.editElements.hitDieSize, 10);
-        Abilities.abilities['constitution'].score = 14;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(12);
-        expect(HitPoints.hitDieSize).toBe(10);
-        expect(HitPoints.constitutionHitPoints).toBe(24);
-        expect(HitPoints.hitPoints).toBe(90);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('90');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('90 (12d10 + 24)');
-      });
-
-      it('swarm of rats', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 7);
-        inputValue(hitPointsSection.editElements.hitDieSize, 8);
-        Abilities.abilities['constitution'].score = 9;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(7);
-        expect(HitPoints.hitDieSize).toBe(8);
-        expect(HitPoints.constitutionHitPoints).toBe(-7);
-        expect(HitPoints.hitPoints).toBe(24);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('24');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('24 (7d8 – 7)');
-      });
-
-      it('tarrasque', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 33);
-        inputValue(hitPointsSection.editElements.hitDieSize, 20);
-        Abilities.abilities['constitution'].score = 30;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(33);
-        expect(HitPoints.hitDieSize).toBe(20);
-        expect(HitPoints.constitutionHitPoints).toBe(330);
-        expect(HitPoints.hitPoints).toBe(676);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('676');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('676 (33d20 + 330)');
-      });
-
-      it('winged kobold', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 3);
-        inputValue(hitPointsSection.editElements.hitDieSize, 6);
-        Abilities.abilities['constitution'].score = 9;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(3);
-        expect(HitPoints.hitDieSize).toBe(6);
-        expect(HitPoints.constitutionHitPoints).toBe(-3);
-        expect(HitPoints.hitPoints).toBe(7);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('7');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('7 (3d6 – 3)');
-      });
-
-      it('minimum values', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 1);
-        inputValue(hitPointsSection.editElements.hitDieSize, 4);
-        Abilities.abilities['constitution'].score = 1;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(1);
-        expect(HitPoints.hitDieSize).toBe(4);
-        expect(HitPoints.constitutionHitPoints).toBe(-5);
-        expect(HitPoints.hitPoints).toBe(0);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('0');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('0 (1d4 – 5)');
-      }); 
-      
-      it('maximum values', () => {
-        inputValue(hitPointsSection.editElements.hitDieQuantity, 999);
-        inputValue(hitPointsSection.editElements.hitDieSize, 20);
-        Abilities.abilities['constitution'].score = 999;
-        hitPointsSection.updateHitPoints();
-
-        expect(HitPoints.useHitDie).toBe(true);
-        expect(HitPoints.hitDieQuantity).toBe(999);
-        expect(HitPoints.hitDieSize).toBe(20);
-        expect(HitPoints.constitutionHitPoints).toBe(493506);
-        expect(HitPoints.hitPoints).toBe(503995);
-
-        expect(hitPointsSection.editElements.hitPoints.value).toBe('503995');
-
-        hitPointsSection.editElements.saveAction.click();
-
-        expect(hitPointsSection).toBeInMode('show');
-        expect(hitPointsSection.showElements.text).toHaveTextContent('503995 (999d20 + 493506)');
-      });  
     });
   });
 
