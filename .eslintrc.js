368902eb1f948ec6b8700c760a13fa1d500bf7c4
Frumple <frumple1@gmail.com>
2019-02-08 13:32:04 -0800

    gitignore: Add coverage directory and remove eslintrc.

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "amd": true,
+        "jest" : true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+      "inputValue": "readable"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2, { "CallExpression": {"arguments": "first"} }
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
