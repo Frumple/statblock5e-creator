47e21d9b6b5bc404944e5d408d6187f68bc899ba
Frumple <frumple1@gmail.com>
2020-04-27 16:54:44 -0700

    Import from JSON: Add drag-and-drop file upload capabilities.

--- src/js/elements/autonomous/dialogs/import-json-dialog.js
+++ src/js/elements/autonomous/dialogs/import-json-dialog.js
@@ -1,5 +1,7 @@
 import ImportDialog from './import-dialog.js';
 
+const dragEnterClass = 'import-json-dialog__file-upload-drop-zone_drag-enter';
+
 export default class ImportJsonDialog extends ImportDialog {
   static get elementName() { return 'import-json-dialog'; }
   static get templatePaths() {
@@ -11,27 +13,79 @@ export default class ImportJsonDialog extends ImportDialog {
   constructor() {
     super(ImportJsonDialog.templatePaths);
 
-    this.chooseFileButton = this.shadowRoot.getElementById('choose-file-button');
     this.fileInput = this.shadowRoot.getElementById('file-input');
+    this.chooseFileButton = this.shadowRoot.getElementById('choose-file-button');
+    this.fileUploadDropZone = this.shadowRoot.getElementById('file-upload-drop-zone');
+
+    // This counter is needed to prevent dragleave events from removing CSS when dragging over child elements in the drop zone.
+    this.dragEnterCounter = 0;
   }
 
   connectedCallback() {
     if (this.isConnected && ! this.isInitialized) {
       super.connectedCallback();
 
-      this.chooseFileButton.addEventListener('click', this.onClickChooseFileButton.bind(this));
       this.fileInput.addEventListener('change', this.onFileSelected.bind(this));
+      this.chooseFileButton.addEventListener('click', this.onClickChooseFileButton.bind(this));
+      this.fileUploadDropZone.addEventListener('dragenter', this.onFileDragEnter.bind(this));
+      this.fileUploadDropZone.addEventListener('dragleave', this.onFileDragLeave.bind(this));
+      this.fileUploadDropZone.addEventListener('dragover', this.onFileDragOver.bind(this));
+      this.fileUploadDropZone.addEventListener('drop', this.onFileDropped.bind(this));
 
       this.isInitialized = true;
     }
   }
 
-  onClickChooseFileButton() {
+  onClickChooseFileButton(event) {
+    event.preventDefault();
+
     this.fileInput.click();
   }
 
-  async onFileSelected() {
+  onFileSelected() {
     const file = this.fileInput.files[0];
+    this.importFile(file);
+  }
+
+  onFileDragEnter() {
+    this.dragEnterCounter++;
+    this.fileUploadDropZone.classList.add(dragEnterClass);
+  }
+
+  onFileDragLeave() {
+    this.dragEnterCounter--;
+    if (this.dragEnterCounter === 0) {
+      this.fileUploadDropZone.classList.remove(dragEnterClass);
+    }
+  }
+
+  onFileDragOver(event) {
+    // Prevent the browser's default behaviour of opening the file when dragging the file over the drop zone.
+    event.preventDefault();
+  }
+
+  onFileDropped(event) {
+    event.preventDefault();
+
+    const items = event.dataTransfer.items;
+
+    if (items) {
+      if (items.length < 1) {
+        this.setStatus('Error: Dragged item contains no data.', 'error');
+      } else if (items.length > 1) {
+        this.setStatus('Error: Cannot upload multiple dragged items.', 'error');
+      } else if (items[0].kind !== 'file') {
+        this.setStatus('Error: Dragged item is not a file.', 'error');
+      } else {
+        const file = items[0].getAsFile();
+        this.importFile(file);
+      }
+    }
+  }
+
+  async importFile(file) {
+    this.setStatus('Uploading file...');
+
     const text = await file.text();
     const json = JSON.parse(text);
 
@@ -43,7 +97,8 @@ export default class ImportJsonDialog extends ImportDialog {
     super.launch(importCallback);
 
     this.fileInput.value = '';
-    this.setStatus('Click "Choose File..." to begin.');
+    this.fileUploadDropZone.classList.remove(dragEnterClass);
+    this.setStatus('');
     this.showModal();
   }
 }
