1169bb1e009040c58fab69aaa22643bb4add8155
Frumple <frumple1@gmail.com>
2021-05-13 19:11:02 -0700

    Implement expression menu functionality.

--- src/js/elements/autonomous/expression-menu.js
+++ src/js/elements/autonomous/expression-menu.js
@@ -10,5 +10,25 @@ export default class ExpressionMenu extends CustomAutonomousElement {
 
   constructor() {
     super(ExpressionMenu.templatePaths);
+
+    this.items = this.shadowRoot.getElementById('expression-menu-items');
+  }
+
+  connectedCallback() {
+    if (this.isConnected && ! this.isInitialized) {
+      super.connectedCallback();
+
+      this.items.addEventListener('click', this.onClickItems.bind(this));
+
+      this.initialized = true;
+    }
+  }
+
+  async onClickItems() {
+    // When an item is clicked, apply the hidden style, wait briefly, then remove the hidden style.
+    // The delay is necessary to ensure that the items are actually hidden.
+    this.items.style.visibility = 'hidden';
+    await new Promise(resolve => setTimeout(resolve, 100));
+    this.items.style.visibility = '';
   }
 }
--- src/js/elements/autonomous/lists/editable-block.js
+++ src/js/elements/autonomous/lists/editable-block.js
@@ -21,6 +21,28 @@ export default class EditableBlock extends DragAndDropListItem {
     this.previewTextElement = this.shadowRoot.getElementById('editable-block-preview-text');
     this.removeButton = this.shadowRoot.getElementById('editable-block-remove-button');
 
+    this.nameButton = this.shadowRoot.getElementById('name-button');
+    this.fullnameButton = this.shadowRoot.getElementById('fullname-button');
+
+    this.modStrButton = this.shadowRoot.getElementById('mod-str-button');
+    this.modDexButton = this.shadowRoot.getElementById('mod-dex-button');
+    this.modConButton = this.shadowRoot.getElementById('mod-con-button');
+    this.modIntButton = this.shadowRoot.getElementById('mod-int-button');
+    this.modWisButton = this.shadowRoot.getElementById('mod-wis-button');
+    this.modChaButton = this.shadowRoot.getElementById('mod-cha-button');
+
+    this.atkStrButton = this.shadowRoot.getElementById('atk-str-button');
+    this.atkDexButton = this.shadowRoot.getElementById('atk-dex-button');
+    this.atkFinButton = this.shadowRoot.getElementById('atk-fin-button');
+
+    this.dmgStrButton = this.shadowRoot.getElementById('dmg-str-button');
+    this.dmgDexButton = this.shadowRoot.getElementById('dmg-dex-button');
+    this.dmgFinButton = this.shadowRoot.getElementById('dmg-fin-button');
+
+    this.sdcIntButton = this.shadowRoot.getElementById('sdc-int-button');
+    this.sdcWisButton = this.shadowRoot.getElementById('sdc-wis-button');
+    this.sdcChaButton = this.shadowRoot.getElementById('sdc-cha-button');
+
     this.dragImage = this.nameInput;
 
     this.isLegendaryActionBlock = false;
@@ -33,6 +55,28 @@ export default class EditableBlock extends DragAndDropListItem {
       this.nameInput.addEventListener('input', this.onInputName.bind(this));
       this.textArea.addEventListener('input', this.onInputText.bind(this));
       this.removeButton.addEventListener('click', this.onClickRemoveButton.bind(this));
+
+      this.nameButton.addEventListener('click', this.onClickNameButton.bind(this));
+      this.fullnameButton.addEventListener('click', this.onClickFullnameButton.bind(this));
+
+      this.modStrButton.addEventListener('click', this.onClickModButton.bind(this, 'STR'));
+      this.modDexButton.addEventListener('click', this.onClickModButton.bind(this, 'DEX'));
+      this.modConButton.addEventListener('click', this.onClickModButton.bind(this, 'CON'));
+      this.modIntButton.addEventListener('click', this.onClickModButton.bind(this, 'INT'));
+      this.modWisButton.addEventListener('click', this.onClickModButton.bind(this, 'WIS'));
+      this.modChaButton.addEventListener('click', this.onClickModButton.bind(this, 'CHA'));
+
+      this.atkStrButton.addEventListener('click', this.onClickAtkButton.bind(this, 'STR'));
+      this.atkDexButton.addEventListener('click', this.onClickAtkButton.bind(this, 'DEX'));
+      this.atkFinButton.addEventListener('click', this.onClickAtkButton.bind(this, 'FIN'));
+
+      this.dmgStrButton.addEventListener('click', this.onClickDmgButton.bind(this, 'STR'));
+      this.dmgDexButton.addEventListener('click', this.onClickDmgButton.bind(this, 'DEX'));
+      this.dmgFinButton.addEventListener('click', this.onClickDmgButton.bind(this, 'FIN'));
+
+      this.sdcIntButton.addEventListener('click', this.onClickSdcButton.bind(this, 'INT'));
+      this.sdcWisButton.addEventListener('click', this.onClickSdcButton.bind(this, 'WIS'));
+      this.sdcChaButton.addEventListener('click', this.onClickSdcButton.bind(this, 'CHA'));
 
       this.isInitialized = true;
     }
@@ -50,6 +94,38 @@ export default class EditableBlock extends DragAndDropListItem {
 
   onClickRemoveButton() {
     this.remove();
+  }
+
+  onClickNameButton() {
+    this.addExpressionToText('[NAME]');
+  }
+
+  onClickFullnameButton() {
+    this.addExpressionToText('[FULLNAME]');
+  }
+
+  onClickModButton(abilityVariable) {
+    this.addExpressionToText(`MOD[${abilityVariable}]`);
+  }
+
+  onClickAtkButton(abilityVariable) {
+    this.addExpressionToText(`ATK[${abilityVariable}]`);
+  }
+
+  onClickDmgButton(abilityVariable) {
+    this.addExpressionToText(`DMG[d6 + ${abilityVariable}]`);
+  }
+
+  onClickSdcButton(abilityVariable) {
+    this.addExpressionToText(`SDC[${abilityVariable}]`);
+  }
+
+  addExpressionToText(expression) {
+    const selectionStart = this.textArea.selectionStart;
+    const selectionEnd = this.textArea.selectionEnd;
+
+    this.textArea.setRangeText(expression, selectionStart, selectionEnd, 'end');
+    this.textArea.focus();
   }
 
   set name(name) {
